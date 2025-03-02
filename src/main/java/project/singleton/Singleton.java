c3caaf9fafceb5e04dbc278ebcedcb491da0e9c2
Guilherme Augustto <guilhermecosta.barros@gmail.com>
2025-03-02 20:21:21 -0300

    feat: add synchronzized to guarantee thread safety

--- src/main/java/project/singleton/Singleton.java
+++ src/main/java/project/singleton/Singleton.java
@@ -1,11 +1,13 @@
 package project.singleton;
 
+package project.singleton;
+
 public class Singleton {
     private static Singleton instancia;
 
     private Singleton() {}
 
-    public static Singleton getInstance() {
+    public static synchronized Singleton getInstance() {
         if (instancia == null) {
             instancia = new Singleton();
         }
